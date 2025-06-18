fb7491e4927cb6b688556fd6d27a10c9490190c4
iqrazartasha33 <iqrazartasha33@gmail.com>
2025-06-18 22:31:43 +0500

    Initial commit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,18 +1,18 @@
-"use client";
 import Link from "next/link";
-import { useEffect } from "react";
 
 function Navbar() {
-  useEffect(() => {
-    const toggleBtn = document.getElementById("menu-toggle");
-    const menu = document.getElementById("mobile-menu");
+  if (typeof window !== "undefined") {
+    setTimeout(() => {
+      const toggleBtn = document.querySelector("#menu-toggle");
+      const menu = document.querySelector("#mobile-menu");
 
-    if (toggleBtn && menu) {
-      toggleBtn.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-      });
-    }
-  }, []);
+      if (toggleBtn && menu) {
+        toggleBtn.addEventListener("click", () => {
+          menu.classList.toggle("hidden");
+        });
+      }
+    }, 0); 
+  }
 
   return (
     <nav className="bg-[#096B68] p-4 shadow-md">
@@ -32,26 +32,17 @@ function Navbar() {
           className="hidden md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 text-center md:items-center md:static absolute top-16 left-0 w-full md:w-auto bg-[#096B68] md:bg-transparent z-50"
         >
           <li className="py-2 md:py-0">
-            <Link
-              href="/"
-              className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4"
-            >
+            <Link href="/" className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4">
               Home
             </Link>
           </li>
           <li className="py-2 md:py-0">
-            <Link
-              href="/about"
-              className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4"
-            >
+            <Link href="/about" className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4">
               About
             </Link>
           </li>
           <li className="py-2 md:py-0">
-            <Link
-              href="/contact"
-              className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4"
-            >
+            <Link href="/contact" className="text-white font-medium hover:text-[#C1DBB3] transition duration-300 block px-4">
               Contact
             </Link>
           </li>
